7624a739ab3e3589bf26f5588fb29783734225e8
Mikhail Gurov <gurov.michal@gmail.com>
2025-01-07 16:39:54 +0100

    ...

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+// Меню
+
+const toggleButton = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+toggleButton.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
